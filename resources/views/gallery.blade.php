bf0aea6d47c34f434a0ed7d825fbc84d4acbdfe5
Angel Garcia <angelrodriguez@ucol.mx>
2020-08-04 15:42:34 -0500

    Gallery

--- resources/views/gallery.blade.php
+++ resources/views/gallery.blade.php
@@ -57,53 +57,39 @@
             </div>
         </div>
     </div>-->
-    <div id="main" class="bg-cover text-white" style="background-image: linear-gradient(to bottom, rgba(0,0,0,0.6) 0%,rgba(0,0,0,0.6) 100%), url('/images/background.jpg');">
+    <div id="gallery" class="bg-cover text-white" style="background-image: linear-gradient(to bottom, rgba(0,0,0,0.6) 0%,rgba(0,0,0,0.6) 100%), url('/images/background.jpg');">
         <div class="container">
             <div class="row">
-                <div class="col-sm-12">
-                    <ul class="social d-flex flex-row justify-content-around">
-                        <li>
-                            <a href="">
-                                <i class="fab fa-facebook-f"></i>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="">
-                                <i class="fab fa-instagram"></i>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="">
-                                <i class="fab fa-whatsapp"></i>
-                            </a>
-                        </li>
-                    </ul>
+                <div class="col-sm-12 my-4">
+                    <h1 class="title text-center">Titulo del Album</h1>
                 </div>
-                <div class="col-sm-12 my-4 d-flex justify-content-center align-items-center">
-                    <h1 class="text-center text-uppercase title">zarazua's photo</h1>
-                </div>
-                <div class="col-sm-12">
-                    <ul class="social d-flex flex-row justify-content-around">
-                        <li>
-                            <a href="{{ url('galeria') }}">
-                                Opción 1
-                            </a>
-                        </li>
-                        <li>
-                            <a href="{{ url('galeria') }}">
-                                Opción 2
-                            </a>
-                        </li>
-                        <li>
-                            <a href="{{ url('galeria') }}">
-                                Opción 3
-                            </a>
-                        </li>
-                    </ul>
+                <div class="col-sm-12 gridBox">
+                    <img src="{{ asset('images/1.jpg') }}" alt="">
+                    <img src="{{ asset('images/2.jpg') }}" alt="">
+                    <img src="{{ asset('images/4.jpg') }}" alt="">
+                    <img src="{{ asset('images/2.jpg') }}" alt="">
+                    <img src="{{ asset('images/2.jpg') }}" alt="">
+                    <img src="{{ asset('images/3.jpg') }}" alt="">
+                    <img src="{{ asset('images/2.jpg') }}" alt="">
+                    <img src="{{ asset('images/3.jpg') }}" alt="">
+                    <img src="{{ asset('images/3.jpg') }}" alt="">
+                    <img src="{{ asset('images/2.jpg') }}" alt="">
+                    <img src="{{ asset('images/1.jpg') }}" alt="">
+                    <img src="{{ asset('images/4.jpg') }}" alt="">
+                    <img src="{{ asset('images/3.jpg') }}" alt="">
+                    <img src="{{ asset('images/3.jpg') }}" alt="">
+                    <img src="{{ asset('images/2.jpg') }}" alt="">
+                    <img src="{{ asset('images/1.jpg') }}" alt="">
+                    <img src="{{ asset('images/3.jpg') }}" alt="">
+                    <img src="{{ asset('images/1.jpg') }}" alt="">
                 </div>
             </div>
         </div>
     </div>
+    <script src="{{ asset('js/jquery.js') }}"></script>
+    <script src="{{ asset('js/app.js') }}"></script>
+    <script>
+    </script>
 </body>
 
 </html>
